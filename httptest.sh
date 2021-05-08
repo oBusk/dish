45cdb9f227f901e020f62f71895a45ed4acd0599
Oscar Busk <oscar.busk@gmail.com>
2021-05-08 03:07:59 +0200

    Move resolving of host into separate file

--- httptest.sh
+++ httptest.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+. functions
+
 resolver=$1
 targetdomain=$2
 
 # Resolve IP of target using resolver
-resolvedIp=$(dig +short $targetdomain @$resolver | tail -n 1)
-# echo -e "resolved ip:\t" $resolvedIp
+resolved_ip=$(resolve_host $targetdomain $resolver)
+echo -e "resolved ip:\t" $resolved_ip
 echo -n "="
 
 # Count number of jumps to IP of target
-distance=$(mtr -nr $resolvedIp | grep -Po '^\s*\d+' | tail -n 1 | xargs)
+distance=$(mtr -nr $resolved_ip | grep -Po '^\s*\d+' | tail -n 1 | xargs)
 # echo -e "distance:\t" $distance
 echo -n "="
 
 # Ping resolved ip
-avgPing=$(ping -c 5 $resolvedIp | grep -Po '(?<=\/)\d+(\.\d+)?(?=\/)' | head -n 1)
-# echo -e "average ping:\t" $avgPing
+avg_ping=$(ping -c 5 $resolved_ip | grep -Po '(?<=\/)\d+(\.\d+)?(?=\/)' | head -n 1)
+# echo -e "average ping:\t" $avg_ping
 echo -ne "=\n"
 
 echo -e "resolv. Ip\tDist.\tPing"
-echo -e $resolvedIp "\t" $distance "\t" $avgPing
+echo -e $resolved_ip "\t" $distance "\t" $avg_ping
