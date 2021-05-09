dba8e625a911d38c06f222eabdb8b152bc5ee23c
Oscar Busk <oscar.busk@gmail.com>
2021-05-09 23:40:26 +0200

    Use dig to lookup ips from url since curl was caching

--- httptest-multi.sh
+++ httptest-multi.sh
@@ -9,46 +9,20 @@ source functions
 URL=$1
 read -d '' INPUT
 
-# Uses Resolver IP ($1) to lookup and follow redirects to get
-#
-# * Effective URL
-#  and
-# * Remote IP
-#
-# Most cases I've tested will only have redirects/CNAME that leads us to the
-# same domain, but just in case there are any differances, we at least take
-# account for it.
-function resolve() {
-	local RESOLVER_IP=$1
-	local URL=$2
+EFFECTIVE_URL=$(get_effective_url $URL)
+EFFECTIVE_HOST=$(get_host $EFFECTIVE_URL)
 
-	CURLOPT_DNS_CACHE_TIMEOUT=0 curl \
-		-I \
-		-s \
-		-o /dev/null \
-		-L \
-		-w '%{remote_ip} %{url_effective}' \
-		--dns-servers 127.0.0.1 \
-		$URL
-}
-
+# A registry of all the remote IPs followed by the resolvers that found that particual IP
 RESULTS=""
-
-# Escapes all forward slashes so that we can find/replace urls with sed command
-sed_escape() {
-	echo ${1} | sed -e "s#/#\\\/#g"
-}
-
 while IFS= read -r RESOLVER_IP || [ -n "${RESOLVER_IP}" ]; do
-	read -r REMOTE_IP EFFECTIVE_URL \
-		<<<$(resolve $RESOLVER_IP $URL)
+	REMOTE_IP=$(resolve_host $EFFECTIVE_HOST $RESOLVER_IP)
 
-	if [[ "$RESULTS" == *"$REMOTE_IP $EFFECTIVE_URL"* ]]; then
-		# Add the current resolver to this ip/effective_url
-		RESULTS=$(sed "/${REMOTE_IP} $(sed_escape $EFFECTIVE_URL)/ s/$/,${RESOLVER_IP}/" <<<$RESULTS)
+	if [[ "$RESULTS" == *"$REMOTE_IP"* ]]; then
+		# Add the current resolver to this ip
+		RESULTS=$(sed "/${REMOTE_IP}/ s/$/,${RESOLVER_IP}/" <<<$RESULTS)
 	else
-		# Create new line for this ip/effective_url with only this resolver
-		RESULTS="${RESULTS}\n${REMOTE_IP} ${EFFECTIVE_URL} ${RESOLVER_IP}"
+		# Create new line for this ip with only this resolver
+		RESULTS="${RESULTS}\n${REMOTE_IP} ${RESOLVER_IP}"
 	fi
 done <<<$INPUT
 
