3de5ecceeef1591a54aa7e1c283f429a1135a923
Oscar Busk <oscar.busk@gmail.com>
2021-05-10 02:07:32 +0200

    Flailing in the dark

--- httptest-multi.sh
+++ httptest-multi.sh
@@ -9,21 +9,43 @@ source functions
 URL=$1
 read -d '' INPUT
 
-EFFECTIVE_URL=$(get_effective_url $URL)
+read EFFECTIVE_URL PORT <<<$(get_effective_url_and_port $URL)
 EFFECTIVE_HOST=$(get_host $EFFECTIVE_URL)
 
 # A registry of all the remote IPs followed by the resolvers that found that particual IP
-RESULTS=""
-while IFS= read -r RESOLVER_IP || [ -n "${RESOLVER_IP}" ]; do
+REMOTE_IPS=""
+while IFS= read -r RESOLVER_IP || [ -n "$RESOLVER_IP" ]; do
 	REMOTE_IP=$(resolve_host $EFFECTIVE_HOST $RESOLVER_IP)
 
-	if [[ "$RESULTS" == *"$REMOTE_IP"* ]]; then
+	if [[ "$REMOTE_IPS" == *"$REMOTE_IP"* ]]; then
 		# Add the current resolver to this ip
-		RESULTS=$(sed "/${REMOTE_IP}/ s/$/,${RESOLVER_IP}/" <<<$RESULTS)
+		REMOTE_IPS=$(sed "/${REMOTE_IP}/ s/$/,${RESOLVER_IP}/" <<<$REMOTE_IPS)
 	else
 		# Create new line for this ip with only this resolver
-		RESULTS="${RESULTS}\n${REMOTE_IP} ${RESOLVER_IP}"
+		REMOTE_IPS="$REMOTE_IPS $REMOTE_IP $RESOLVER_IP
+"
 	fi
-done <<<$INPUT
+done <<<"$INPUT"
 
-echo -e $RESULTS
+# Remove last line
+REMOTE_IPS=$(head -n -1 <<<"$REMOTE_IPS")
+
+# readarray -t LINES <<<"$REMOTE_IPS"
+
+while read -r REMOTE_IP RESOLVERS REST || [ -n "$REMOTE_IP" ]; do
+	DISTANCE=$(count_hops $REMOTE_IP)
+	LOCATION=$(ip_location $REMOTE_IP)
+	TIME_PRETRANSFER=$(curl \
+		-w '%{time_pretransfer}' \
+		-I \
+		-s \
+		-o /dev/null \
+		--resolve "$EFFECTIVE_HOST:$PORT:$REMOTE_IP" \
+		$EFFECTIVE_URL)
+
+	REMOTE_IPS=$(sed "/${REMOTE_IP}/ s/$/ ${LOCATION} ${DISTANCE} $(s_to_ms $TIME_PRETRANSFER)/" <<<$REMOTE_IPS)
+
+	echo "[$REMOTE_IP]($RESOLVERS) Location: $LOCATION, Distance: $DISTANCE, Pre-transfer: $(s_to_ms $TIME_PRETRANSFER)"
+done <<<"$REMOTE_IPS"
+
+# echo -e $REMOTE_IPS
