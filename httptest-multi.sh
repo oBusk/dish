716ee4e1e773df5132e47a3fa43461561002b4bf
Oscar Busk <oscar.busk@gmail.com>
2021-05-10 23:21:01 +0200

    Cache resolving, count hops and location.
    Not sure if this is wanted behaviour, but at least makes development a lot easier to test

--- httptest-multi.sh
+++ httptest-multi.sh
@@ -15,14 +15,11 @@ EFFECTIVE_HOST=$(get_host $EFFECTIVE_URL)
 # A registry of all the remote IPs followed by the resolvers that found that particual IP
 REMOTE_IPS=""
 while IFS= read -r RESOLVER_IP || [ -n "$RESOLVER_IP" ]; do
-	REMOTE_IP=$(resolve_host $EFFECTIVE_HOST $RESOLVER_IP)
+	REMOTE_IP=$(resolve_host_with_cache $EFFECTIVE_HOST $RESOLVER_IP)
 
-	if [[ "$REMOTE_IPS" == *"$REMOTE_IP"* ]]; then
-		# Add the current resolver to this ip
-		REMOTE_IPS=$(sed "/${REMOTE_IP}/ s/$/,${RESOLVER_IP}/" <<<$REMOTE_IPS)
-	else
+	if [[ ! "$REMOTE_IPS" == *"$REMOTE_IP"* ]]; then
 		# Create new line for this ip with only this resolver
-		REMOTE_IPS="$REMOTE_IPS $REMOTE_IP $RESOLVER_IP
+		REMOTE_IPS="$REMOTE_IPS $REMOTE_IP
 "
 	fi
 done <<<"$INPUT"
@@ -30,22 +27,14 @@ done <<<"$INPUT"
 # Remove last line
 REMOTE_IPS=$(head -n -1 <<<"$REMOTE_IPS")
 
-# readarray -t LINES <<<"$REMOTE_IPS"
-
-while read -r REMOTE_IP RESOLVERS REST || [ -n "$REMOTE_IP" ]; do
-	DISTANCE=$(count_hops $REMOTE_IP)
-	LOCATION=$(ip_location $REMOTE_IP)
-	TIME_PRETRANSFER=$(curl \
-		-w '%{time_pretransfer}' \
-		-I \
-		-s \
-		-o /dev/null \
-		--resolve "$EFFECTIVE_HOST:$PORT:$REMOTE_IP" \
-		$EFFECTIVE_URL)
+while read -r REMOTE_IP REST || [ -n "$REMOTE_IP" ]; do
+	DISTANCE=$(count_hops_with_cache $REMOTE_IP)
+	LOCATION=$(ip_location_with_cache $REMOTE_IP)
+	TIME_PRETRANSFER=$(time_pretransfer $REMOTE_IP $EFFECTIVE_HOST $PORT $EFFECTIVE_URL)
 
 	REMOTE_IPS=$(sed "/${REMOTE_IP}/ s/$/ ${LOCATION} ${DISTANCE} $(s_to_ms $TIME_PRETRANSFER)/" <<<$REMOTE_IPS)
 
-	echo "[$REMOTE_IP]($RESOLVERS) Location: $LOCATION, Distance: $DISTANCE, Pre-transfer: $(s_to_ms $TIME_PRETRANSFER)"
+	echo "[$REMOTE_IP] Location: $LOCATION, Distance: $DISTANCE, Pre-transfer: $(s_to_ms $TIME_PRETRANSFER)"
 done <<<"$REMOTE_IPS"
 
 # echo -e $REMOTE_IPS
