d90da771c35b23c9804ed7ded34911a8b3198426
Oscar Busk <oscar.busk@gmail.com>
2021-05-13 16:50:26 +0200

    Add basic headings for CSV

--- benchmark-multiple.sh
+++ benchmark-multiple.sh
@@ -13,5 +13,7 @@ while read -r RESOLVER_IP R || [ -n "$RESOLVER_IP" ]; do
 	append_line "$(source benchmark.sh $RESOLVER_IP)"
 done <<<"$INPUT"
 
-printf "Sep=; $(sort -t ";" -k5 -n <<<$RESULT)
+printf "Sep=;
+DNS Ip; whoami 'ns'; whoami 'ecs'; whoami 'ip'; Akamai Edge; Akamai Edge Location; Akamai Edge Distance; Akamai Edge Pretransfer
+$(sort -t ";" -k8 -n <<<$RESULT)
 "
