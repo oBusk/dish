fe8ad1ae3eca8c90ba8e697a3e9ae99a739fc373
Oscar Busk <oscar.busk@gmail.com>
2021-05-16 01:56:08 +0200

    Stop outputting "ms" in timing

--- functions/util.sh
+++ functions/util.sh
@@ -3,7 +3,7 @@ source functions/cache.sh
 
 # Takes seconds and outputs it as ms with a single decimal place
 function s_to_ms() {
-    echo "$(echo "scale=1;${1}*1000/1" | bc)ms"
+    echo "$(echo "scale=1;${1}*1000/1" | bc)"
 }
 
 # Escapes all forward slashes so that we can find/replace urls with sed command
