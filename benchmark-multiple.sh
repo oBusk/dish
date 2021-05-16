1528bca0108977055208980f2bebd7fb42222f4f
Oscar Busk <oscar.busk@gmail.com>
2021-05-16 16:27:01 +0200

    Stop sorting before outputting

--- benchmark-multiple.sh
+++ benchmark-multiple.sh
@@ -4,16 +4,17 @@
 
 read -d '' INPUT
 
-function append_line() {
-	RESULT="$RESULT
-$1"
-}
+# function append_line() {
+# 	RESULT="$RESULT
+# $1"
+# }
+
+echo "Sep=;"
+echo "DNS Ip; hostname; DNS Organization; whoami 'ns'; ns location; ns distance; whoami 'ecs'; whoami 'ip'; Akamai Edge; Akamai Edge Location; Akamai Edge Distance; Akamai Edge Pretransfer; Netflix Edge; Netflix Edge Location; Netflix Edge Distance; Netflix Edge Pretransfer; Youtube Edge; Youtube Edge Location; Youtube Edge Distance; Youtube Edge Pretransfer"
 
 while read -r RESOLVER_IP R || [ -n "$RESOLVER_IP" ]; do
-	append_line "$(source benchmark.sh $RESOLVER_IP)"
+	#append_line "$(source benchmark.sh $RESOLVER_IP)"
+	echo "$(source benchmark.sh $RESOLVER_IP)"
 done <<<"$INPUT"
 
-printf "Sep=;
-DNS Ip; hostname; DNS Organization; whoami 'ns'; ns location; ns distance; whoami 'ecs'; whoami 'ip'; Akamai Edge; Akamai Edge Location; Akamai Edge Distance; Akamai Edge Pretransfer; Netflix Edge; Netflix Edge Location; Netflix Edge Distance; Netflix Edge Pretransfer; Youtube Edge; Youtube Edge Location; Youtube Edge Distance; Youtube Edge Pretransfer
-$(sort -t ";" -k11 -n <<<$RESULT)
-"
+# echo "$(sort -t ";" -k11 -n <<<$RESULT)"
