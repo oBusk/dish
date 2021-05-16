dc1a522a76059d72dafa851af6c25218e46f5b02
Oscar Busk <oscar.busk@gmail.com>
2021-05-16 15:53:45 +0200

    Fix mess of dns-location/ip-location working together

--- benchmarks/dns-location.sh
+++ benchmarks/dns-location.sh
@@ -5,11 +5,12 @@ source functions/count-hops.sh
 
 RESOLVER_IP=$1
 
+IFS=$'\t' read -r NS_HOSTNAME NS_ORGANIZATION NS_LOCATION <<<$(ip_location_with_cache $RESOLVER_IP)
 read NS ECS IP <<<$(who_am_i_with_cache $RESOLVER_IP)
 
 if [[ -n "$NS" ]]; then
-    NS_LOCATION=$(ip_location_with_cache $NS)
+    IFS=$'\t' read -r WHOAMI_HOSTNAME WHOAMI_ORGANIZATION WHOAMI_LOCATION <<<$(ip_location_with_cache $NS)
     NS_DISTANCE=$(count_hops_with_cache $NS)
 fi
 
-echo "$NS; $NS_LOCATION; $NS_DISTANCE; $ECS; $IP"
+echo "$NS_HOSTNAME; $NS_ORGANIZATION; $NS; $WHOAMI_LOCATION; $NS_DISTANCE; $ECS; $IP"
--- functions/ip-location.sh
+++ functions/ip-location.sh
@@ -4,22 +4,22 @@ source functions/cache.sh
 IP_LOCATION_CACHE="$CACHE_DIR/ip-location.txt"
 
 function ip_location_with_cache() {
-    local REMOTE_IP=$1
+	local REMOTE_IP=$1
 
-    ensure_cache_file_exists $IP_LOCATION_CACHE
+	ensure_cache_file_exists $IP_LOCATION_CACHE
 
-    MATCHING_LINE=$(grep "^$REMOTE_IP " <$IP_LOCATION_CACHE)
-    if [[ -n "$MATCHING_LINE" ]]; then
-        read -d ";" R HOSTNAME ORGANIZATION LOCATION <<<$MATCHING_LINE
-    else
-        read -d ";" HOSTNAME ORGANIZATION LOCATION <<<$(ip_location $REMOTE_IP)
+	MATCHING_LINE=$(grep "^$REMOTE_IP\\t" <$IP_LOCATION_CACHE)
+	if [[ -n "$MATCHING_LINE" ]]; then
+		IFS=$'\t' read -r R HOSTNAME ORGANIZATION LOCATION <<<$MATCHING_LINE
+	else
+		IFS=$'\t' read -r HOSTNAME ORGANIZATION LOCATION <<<$(ip_location $REMOTE_IP)
 
-        echo -e "$REMOTE_IP;$HOSTNAME;$ORGANIZATION;$LOCATION" >>$IP_LOCATION_CACHE
-    fi
+		echo -e "$REMOTE_IP	$HOSTNAME	$ORGANIZATION	$LOCATION" >>$IP_LOCATION_CACHE
+	fi
 
-    echo "$HOSTNAME; $ORGANIZATION; $LOCATION"
+	echo -e "$HOSTNAME	$ORGANIZATION	$LOCATION"
 }
 
 function ip_location() {
-    curl -s ipinfo.io/$1 | jq -j '.hostname // empty, ";", .org, ";", .city, ", ", .country'
+	curl -s ipinfo.io/$1 | jq -j '.hostname, "	", .org, "	", .city, ", ", .country'
 }
