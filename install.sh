867af3b6a81266796ae171ca9cc524a2b76e622c
Oscar Busk <oscar.busk@gmail.com>
2021-05-18 01:07:14 +0200

    Cleanup by making fetch-dnsdiag into a function

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+source functions/fetch-dnsdiag.sh
 
-sudo apt-get update &&
-    sudo apt-get install jq
-
-source fetch-dnsdiag.sh
+sudo apt-get update
+sudo apt-get install jq
+fetch_dnsdiag
