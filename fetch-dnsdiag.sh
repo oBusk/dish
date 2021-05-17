49289724c8e9d94f5724dabdac00e42c7bf83a2f
Oscar Busk <oscar.busk@gmail.com>
2021-05-18 00:46:55 +0200

    Add simplistic script to download dnsdiag into directory

---
+++ fetch-dnsdiag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir .dnsdiag
+curl -s https://api.github.com/repos/farrokhi/dnsdiag/releases/latest |
+    grep "browser_download_url.*linux-x86_64-bin" |
+    cut -d : -f 2,3 |
+    tr -d \" |
+    wget -i - -O - |
+    tar -xz -C .dnsdiag --strip-components=1
--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 sudo apt-get update &&
     sudo apt-get install jq
+
+source fetch-dnsdiag.sh
