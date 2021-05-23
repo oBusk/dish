39e852e9a3c48c6a09d61f80819a1c9b10d8b49e
Oscar Busk <oscar.busk@gmail.com>
2021-05-24 01:40:26 +0200

    Fix pretransfer to return 999 instead of 0

--- functions/time-pretransfer.sh
+++ functions/time-pretransfer.sh
@@ -44,11 +44,19 @@ function time_pretransfer() {
     local PORT=$3
     local URL=$4
 
-    curl \
-        -w '%{time_pretransfer}' \
-        -I \
-        -s \
-        -o /dev/null \
-        --resolve "$EFFECTIVE_HOST:$PORT:$REMOTE_IP" \
-        $EFFECTIVE_URL
+    local TIME_PRETRANSFER=$(
+        curl \
+            -w '%{time_pretransfer}' \
+            -I \
+            -s \
+            -o /dev/null \
+            --resolve "$EFFECTIVE_HOST:$PORT:$REMOTE_IP" \
+            $EFFECTIVE_URL
+    )
+
+    if [[ -n "$TIME_PRETRANSFER" ]]; then
+        echo "999"
+    else
+        echo "$TIME_PRETRANSFER"
+    fi
 }
